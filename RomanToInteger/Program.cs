5862a3219a931c5ba65a41bc76a643ed0dcf490e
Paulo Henrique Ziemer dos Santos <zs.phenriqueprofessional@gmail.com>
2024-10-28 09:45:31 -0300

    roman to integer.

---
+++ RomanToInteger/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+public class Program{
+    public static void Main(){
+        int value = RomanToInt("LVIII");
+        System.Console.WriteLine(value);
+    }
+
+    public static int RomanToInt(string romanNumber){
+         Dictionary<char, int> relation = new Dictionary<char, int>
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+        int result = 0;
+        int previous = 0;
+        
+        for(int i = romanNumber.Length-1; i >= 0; i--){
+
+            int current = relation[romanNumber[i]];
+
+            if(current < previous)
+                result -= current;
+            else
+                result += current;
+            
+            previous = current;
+        }
+        return result;
+    }
+}
